6a80fedb858ce57931020cbbaf65ba369ddbf34c
jonatan@routing.is <jonatan@routing.is>
2019-11-09 20:51:54 +0000

    token refresh n stuff

--- fdm.py
+++ fdm.py
@@ -28,3 +28,23 @@ def getNewTokens():
  accessTokens['datetime'] = datetime.today()
 
 
+def checkTokenValidity():
+ if (datetime.today() - accessTokens['datetime']).seconds > accessTokens['expires_in']:
+  print('token expiired')
+ if (datetime.today() - accessTokens['datetime']).seconds > accessTokens['refresh_expires_in']:
+  print('refresh token expiired')
+
+
+def updateHeaders():
+ headers['Authorization'] = 'Bearer ' + accessTokens['access_token']
+
+def refreshToken():
+ fdm_tokenrefresh_grant = {
+  'grant_type': 'refresh_token',
+  'refresh_token': accessTokens['refresh_token']
+ }
+ response = requests.post('https://' + cfg.fdm_ip + '/api/fdm/latest/fdm/token', data=json.dumps(fdm_tokenrefresh_grant), headers=headers, verify=False)
+ responseJSON = json.loads(bytes.decode(response.content))
+ for item in responseJSON:
+  accessTokens[item] = responseJSON[item]
+ accessTokens['datetime'] = datetime.today()
